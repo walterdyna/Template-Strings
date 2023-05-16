aaf00e5ff7448f0623293591aef9832c64360c1e
walterdyna <alexdyna@gmail.com>
2023-05-15 22:36:45 -0300

    textos + strings

---
+++ Template Strings.js
@@ -0,0 +1,14 @@
+
+
+
+const myAge = 30
+const myCar = `Auldi`
+
+//                  texto      javascritp
+const myString = `Minha idade e ${myAge}, to ficando velho.
+Mas pelo menos to andando de ${myCar}` // posso juntar texto com string
+
+console.log(myString)
+
+
+
